0c66e59bf0c7546ebeceef462df3a185c1d126fe
Y.Lin <yl339@myrtle.kent.ac.uk>
2021-12-15 03:08:23 +0000

    retraining debug

--- retrain_run.sh
+++ retrain_run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH -o ./logs/%j.log
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=yl339@kent.ac.uk
 THEANO_FLAGS='device=cuda,floatX=float32'
 
 date -d -30days
 
-python /cluster/home/cug/yl339/current_proj/src/retrain.py 0
-python /cluster/home/cug/yl339/current_proj/src/retrain.py 1
-python /cluster/home/cug/yl339/current_proj/src/retrain.py 2
-python /cluster/home/cug/yl339/current_proj/src/retrain.py 3
+python /cluster/home/cug/yl339/TCN-proj/src/retrain.py 0
 
 date -d -30days
+
