ab1bd07c8eaefaa40b7c61fae9fa0e6d0c457a5e
YuzhouLin <yl339@kent.ac.uk>
2022-03-05 01:37:36 +0000

    redesigned retraining process

---
+++ src/acc_check.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import os
+
+
+## Functions for calculating accuracy
+def get_acc(model, sb, valid_trials, read_folder="/results"):
+    
+    day_n_list = []
+    time_n_list = []
+    trial_n_list = []
+    acc_list = []
+    
+    for day_n in range(1,6):
+        for t_n in range(1,3):
+            for T_n in range(1,13):
+                tmp_R = pd.read_csv(os.getcwd()+f"{read_folder}/{model}/sb{sb}d{day_n}_t{t_n}_T{T_n}.csv")
+                day_n_list.append(day_n)
+                time_n_list.append('AM') if t_n == 1 else time_n_list.append('PM')
+                #time_n_list.append(t_n)
+                if day_n == 1 and T_n not in valid_trials:
+                    continue
+                trial_n_list.append(T_n)
+                acc_list.append(np.sum(tmp_R['actual'] == tmp_R['predict'])/len(tmp_R['predict']))
+    
+    n = len(acc_list)
+    acc_dict={
+            'model': [model]*n,
+            'sb': [sb]*n,
+            'day': day_n_list,
+            'time': time_n_list,
+            'trial': trial_n_list,
+            'acc': acc_list
+    }
+    
+    #df_new = pd.DataFrame(acc_dict, index=np.arange(0,n,1))
+    df = pd.DataFrame(acc_dict)
+    return df
+
+
+if __name__ == "__main__":
+    
+    folder = "/results"
+    filename = './analysis/acc_report_temp.csv'
+    models = ['ecnn0']
+    sb_n = 2
+    #models = ['ecnn0', 'ecnn1', 'ecnn2', 'ecnn3', 'etcn0', 'etcn1', 'etcn2', 'etcn3']
+
+    for i in models:
+        df_new = get_acc(model=i, sb=sb_n, read_folder=folder)
+        if os.path.exists(filename):
+            print('Update new')
+            df = pd.read_csv(filename)
+            #df = df.append(df_new, ignore_index=True)
+            df = pd.concat([df, df_new], ignore_index=True)
+        else:
+            print('Create new')
+            df = df_new
+        df.to_csv(filename, index=False)
+        
+    df_acc = pd.read_csv(filename)
+    df_acc_summary = df_acc.groupby(by=['day', 'time','model']).mean().unstack()
+    print(df_acc_summary['acc'])
--- src/retrain.py
+++ src/retrain.py
@@ -7,6 +7,8 @@ import os
 import time
 import yaml
 from easydict import EasyDict as edict
+import numpy as np
+import pandas as pd
 
 #from torch.utils.tensorboard import SummaryWriter
 
@@ -27,7 +29,7 @@ DEVICE = pre.try_gpu()
 # check if this is the most case, if so, train 1 epoch or few epochs instead
 
 
-def retrain(cfg):
+def retrain(cfg): # kind of transfer learning
     # load_data
     train_trail_list = list(range(1,cfg.DATA_CONFIG.trial_n+1))
 
@@ -80,16 +82,87 @@ def retrain(cfg):
     return
 
 
+def retraining(cfg): # retrained from scratch
+    fold = 4
+    train_trial_list = list(set(range(1,cfg.DATA_CONFIG.trial_n+1))-set(cfg.CV.valid_trial_list[fold]))
+
+    # load_data
+    train_loader = pre.load_data(cfg.DATA_PATH, cfg.DATA_CONFIG.sb_n, cfg.DATA_CONFIG.day_list, cfg.DATA_CONFIG.time_list, train_trial_list, tcn_used=TCN_USED, batch_size=cfg.HP.batch_size, shuffle=cfg.DATA_LOADER.shuffle, drop_last=cfg.DATA_LOADER.drop_last, num_workers=cfg.DATA_LOADER.num_workers, pin_memory=cfg.DATA_LOADER.pin_memory)
+    
+    valid_loader = pre.load_data(cfg.DATA_PATH, cfg.DATA_CONFIG.sb_n, cfg.DATA_CONFIG.day_list, cfg.DATA_CONFIG.time_list, cfg.CV.valid_trial_list[fold], tcn_used=TCN_USED, batch_size=cfg.HP.batch_size, shuffle=cfg.DATA_LOADER.shuffle, drop_last=cfg.DATA_LOADER.drop_last, num_workers=cfg.DATA_LOADER.num_workers, pin_memory=cfg.DATA_LOADER.pin_memory)
+
+    trainloaders = {
+        "train": train_loader,
+        "val": valid_loader,
+    }
+
+    n_class = len(cfg.CLASS_NAMES)
+    # Load Model
+    if TCN_USED:
+        model = utils.TCN(input_size=cfg.DATA_CONFIG.channel_n, output_size=n_class, num_channels=cfg.HP.tcn_channels, kernel_size=cfg.HP.kernel_size, dropout=cfg.HP.dropout_rate)
+    else:
+        model = utils.Model(number_of_class=n_class, dropout=cfg.HP.dropout_rate)
+    model.to(DEVICE)
+    optimizer = getattr(
+        torch.optim,cfg.HP.optimizer)(model.parameters(), lr=cfg.HP.lr)
+
+    eng = utils.EngineTrain(model, optimizer, device=DEVICE)
+    
+    loss_params = {'edl_used': EDL_USED, 'device': DEVICE}
+    if EDL_USED != 0:
+        loss_params['class_n'] = n_class
+        for item in cfg.HP_SEARCH[f'EDL{EDL_USED}']:
+            loss_params[item]=cfg.HP[item]
+
+    best_loss = np.inf
+    early_stopping_iter = cfg.TRAINING.early_stopping_iter
+
+    for epoch in range(1, cfg.TRAINING.epochs + 1): 
+        # if 'annealing_step' in loss_params:
+        if EDL_USED == 2:
+            loss_params['epoch_num'] = epoch
+        train_losses, acc = eng.train(trainloaders, loss_params)
+        train_loss = train_losses['train']
+        valid_loss = train_losses['val']
+        train_acc = acc['train']
+        valid_acc = acc['val']
+
+        print(
+            f"epoch:{epoch}, "
+            f"train_loss: {train_loss}, "
+            f"valid_loss: {valid_loss}. "
+            f"train_acc: {train_acc}, "
+            f"valid_acc: {valid_acc}. "
+        )
+
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            early_stopping_counter = 0
+            
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'best_loss': best_loss # best_loss
+                }, cfg.model_path+f'/{cfg.RETRAINING.model_name}_sb{cfg.DATA_CONFIG.sb_n}.pt')
+            
+        else:
+            early_stopping_counter += 1
+        if early_stopping_counter > early_stopping_iter:
+            break
+    
+    return best_loss
+
 
 if __name__ == "__main__":
     # Load config file from hpo search
     with open("hpo_search.yaml", 'r') as f:
         cfg = edict(yaml.load(f, Loader=yaml.SafeLoader))
-        
+    
+    
+    sb_n = cfg.DATA_CONFIG.sb_n
     # Load determined optimal hyperparameters
     study_dir = f'etcn{EDL_USED}' if TCN_USED else f'ecnn{EDL_USED}'
     study_path = os.getcwd() + cfg.STUDY_PATH + study_dir
-    with open(f'{study_path}/sb_{cfg.DATA_CONFIG.sb_n}', 'r') as f:
+    with open(f'{study_path}/sb_{sb_n}', 'r') as f:
         hp = yaml.load(f, Loader=yaml.SafeLoader)
     
     #cfg['best_loss'] = hp[0]['best_loss']
@@ -100,4 +173,5 @@ if __name__ == "__main__":
     cfg.model_path = os.getcwd() + cfg.MODEL_PATH + study_dir
     #cfg.TRAINING.epochs = 10
     # retraining and save the models
-    retrain(cfg)
+    #retrain(cfg)
+    retraining(cfg)
