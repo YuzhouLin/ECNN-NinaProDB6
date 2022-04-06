5c35ec6c66eaa40ebdd3fdd09988fb07c9628178
YuzhouLin <yl339@kent.ac.uk>
2022-04-06 14:50:52 +0100

    normalised weights done

--- normalised_weights.py
+++ normalised_weights.py
@@ -12,24 +12,22 @@ parser.add_argument('--sb', type=int)
 
 args=parser.parse_args()
 
-def seg_emg(samples, wl=400, ratio_non_overlap=0.1):
-    segmented = []
-    for n in range(0, samples.shape[0]-wl, round(wl*ratio_non_overlap)):
-        segdata = samples[n:n+wl,:] # 400*14
-        segmented.append(np.expand_dims(segdata, axis=0))
-    return segmented
 
 if __name__ == "__main__":
     sb_n = args.sb
     DATA_PATH = f'../../hy-nas/Data6/s{sb_n}'
-    
+
     train_folder = DATA_PATH+'/train'
     W_train = np.load(train_folder+'/W.npy')
-    Y_train = np.load(val_folder+'/Y.npy')
-    
+    Y_train = np.load(train_folder+'/Y.npy')
+
     val_folder = DATA_PATH+'/val'
     W_val = np.load(val_folder+'/W.npy')
     Y_val=np.load(val_folder+'/Y.npy')
-    
 
-    
+    for i in range(8):
+        W_train[Y_train==i] = (W_train[Y_train==i] - 1.8)/(W_train[Y_train==i].max()-1.8)
+        W_val[Y_val==i] = (W_val[Y_val==i] - 1.8)/(W_val[Y_val==i].max()-1.8)
+
+    np.save(train_folder+'W_n.npy')
+    np.save(val_folder+'W_n.npy')
